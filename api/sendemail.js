3bbd39ac69843895b28f4d6092cf96c976ea471c
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 20:50:48 +0100

    debug request

--- api/sendemail.js
+++ api/sendemail.js
@@ -2,7 +2,9 @@ module.exports = async (req, res) => {
   // this function will be launched from Hasura event
   try {
     if (req.method === "GET") {
-      res.status(200).json({ message: "email sent to the user!" });
+      console.log(req);
+      res.status(200).json(req);
+      //res.status(200).json({ message: "email sent to the user!" });
     } else {
       res.status(200).json({ message: "NO PERMISSION TO USE THIS!" });
     }
