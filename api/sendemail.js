1d02a1dab58ab70595befbb37e14860556557470
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 21:29:20 +0100

    const request = JSON.parse(req.body);

--- api/sendemail.js
+++ api/sendemail.js
@@ -2,8 +2,8 @@ module.exports = async (req, res) => {
   // this function will be launched from Hasura event
   try {
     if (req.method === "POST") {
-      console.log(req.payload);
-      res.status(200).json({ req1: req, body1: req.body });
+      const request = JSON.parse(req.body);
+      res.status(200).json( request.event.data );
     } else {
       res.status(200).json({ message: "NO PERMISSION TO USE THIS!" });
     }
