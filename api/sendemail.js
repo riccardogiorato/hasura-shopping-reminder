3a6c790dcbeeefc82ef6f0fe602a9cf057005f79
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 22:05:49 +0100

    debug body

--- api/sendemail.js
+++ api/sendemail.js
@@ -1,16 +1,13 @@
 module.exports = (req, res) => {
   // this function will be launched from Hasura event
+  const { body } = req
   try {
-    const {
-      event: { op, data },
-      table: { name, schema },
-    } = JSON.parse(req.body);
     res
       .status(200)
       .json({
         method: req.method,
         msg: "Received event!",
-        data: { op, data, name, schema },
+        data: { body },
       });
   } catch (err) {
     res.send(err); // send the thrown error
