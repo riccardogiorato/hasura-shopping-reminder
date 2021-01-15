208a68b01718c7764dccb749b6107dc873844cf8
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 21:19:15 +0100

    check payload

--- api/sendemail.js
+++ api/sendemail.js
@@ -1,9 +1,9 @@
 module.exports = async (req, res) => {
   // this function will be launched from Hasura event
   try {
-    if (req.method === "GET") {
-      console.log(req);
-      res.status(200).json(req);
+    if (req.method === "POST") {
+      console.log(req.payload);
+      res.status(200).json(req.payload);
       //res.status(200).json({ message: "email sent to the user!" });
     } else {
       res.status(200).json({ message: "NO PERMISSION TO USE THIS!" });
