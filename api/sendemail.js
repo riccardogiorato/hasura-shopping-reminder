e84955d10f28703fa53f189546d0745f593a6859
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 21:24:21 +0100

    test req.body

--- api/sendemail.js
+++ api/sendemail.js
@@ -3,8 +3,7 @@ module.exports = async (req, res) => {
   try {
     if (req.method === "POST") {
       console.log(req.payload);
-      res.status(200).json(req.payload);
-      //res.status(200).json({ message: "email sent to the user!" });
+      res.status(200).json({ req1: req, body1: req.body });
     } else {
       res.status(200).json({ message: "NO PERMISSION TO USE THIS!" });
     }
