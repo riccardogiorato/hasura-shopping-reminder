f00cb0ea14f14f149f141b41436b06e78fb16446
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 21:55:44 +0100

    req.method

--- api/sendemail.js
+++ api/sendemail.js
@@ -1,12 +1,17 @@
 module.exports = async (req, res) => {
   // this function will be launched from Hasura event
   try {
-    if (req.method === "POST") {
-      const request = JSON.parse(req.body);
-      res.status(200).json( request.event.data );
-    } else {
-      res.status(200).json({ message: "NO PERMISSION TO USE THIS!" });
-    }
+    const {
+      event: { op, data },
+      table: { name, schema },
+    } = JSON.parse(req.body);
+    res
+      .status(200)
+      .json({
+        method: req.method,
+        msg: "Received event!",
+        data: { op, data, name, schema },
+      });
   } catch (err) {
     res.send(err); // send the thrown error
   }
