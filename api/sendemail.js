10a585361182994ea3b0625b02f0f989311ab77a
Riccardo Giorato <giorat.96@gmail.com>
2021-01-15 08:57:18 +0100

    Update sendemail.js

--- api/sendemail.js
+++ api/sendemail.js
@@ -1,10 +1,12 @@
-
-module.exports = async (req, res) => { // this function will be launched from Hasura event
-    try {
-        if (req.method === 'POST') {
-            res.status(200).json({message: "email sent to the user!"});
-          }
-    } catch (err) {
-      res.send(err) // send the thrown error
+module.exports = async (req, res) => {
+  // this function will be launched from Hasura event
+  try {
+    if (req.method === "GET") {
+      res.status(200).json({ message: "email sent to the user!" });
+    } else {
+      res.status(200).json({ message: "NO PERMISSION TO USE THIS!" });
     }
+  } catch (err) {
+    res.send(err); // send the thrown error
   }
+};
